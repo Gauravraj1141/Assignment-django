86276c166b3a49a23136a780d87e3dbc59d2743d
Gaurav Rajput <105104456+Gauravraj1141@users.noreply.github.com>
2022-12-31 17:59:08 +0530

    update

--- grapp/sender.py
+++ grapp/sender.py
@@ -6,12 +6,10 @@ import random
 def send_otp_to_phone(phone_number):
     try:
         otp = random.randint(1000, 9999)
-        # api_key = settings.API_KEY
-        # url = f'https://2factor.in/API/V1/{api_key}/SMS/{phone_number}/{otp}'
-        # response = requests.get(url)
-        # print(response)
-        # print(response)
-        print(otp)
+        api_key = settings.API_KEY
+        url = f'https://2factor.in/API/V1/{api_key}/SMS/{phone_number}/{otp}'
+        response = requests.get(url)
+
         return otp
     except Exception as e:
         print(e)
